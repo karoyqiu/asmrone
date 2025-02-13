e3345e32880ba61cc46e2b4767ede666c37349fe
Roy QIU <karoyqiu@gmail.com>
2025-02-13 09:24:24 +0800

    添加基础配置。

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    asmrone_lib::run()
+  asmrone_lib::run()
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+import App from './App';
+
+if (!import.meta.env.DEV) {
+  document.addEventListener('contextmenu', (event) => {
+    if (!event.target || !('tagName' in event.target) || event.target.tagName !== 'INPUT') {
+      event.preventDefault();
+    }
+  });
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
