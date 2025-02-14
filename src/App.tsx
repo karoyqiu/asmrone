fd06c185485a21f5f555b99b218d9f4cff3d4775
Roy QIU <karoyqiu@gmail.com>
2025-02-14 17:18:55 +0800

    记录下载完成时间。

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ import { IconField } from 'primereact/iconfield';
 import { InputIcon } from 'primereact/inputicon';
 import { InputText } from 'primereact/inputtext';
 import { ScrollPanel } from 'primereact/scrollpanel';
+import { Toast } from 'primereact/toast';
 import type { TreeCheckboxSelectionKeys } from 'primereact/tree';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 import './App.css';
 import { Track, download, getTracks } from './lib/asmrone';
@@ -43,12 +44,40 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [tracks, setTracks] = useState<Track[]>([]);
   const [checked, setChecked] = useState<TreeCheckboxSelectionKeys | null>(null);
+  const [child, setChild] = useState<Child>();
   const [settingsVisible, setSettingsVisible] = useState(false);
   const [dir] = useLocalStorage('', 'dir');
   const [proxy] = useLocalStorage('', 'proxy');
   const [proxyOnDownload] = useLocalStorage(false, 'proxyOnDownload');
-  const [child, setChild] = useState<Child>();
+  const [records, setRecords] = useLocalStorage<Record<string, number>>({}, 'records');
+  const toast = useRef<Toast>(null);
+
   const hasChecked = !!checked && Object.values(checked).some((value) => value.checked);
+
+  const id = useMemo(() => {
+    let id = rjid.toUpperCase();
+
+    if (id.startsWith('RJ')) {
+      id = id.substring(2);
+    }
+
+    return id;
+  }, [rjid]);
+
+  useEffect(() => {
+    if (toast.current) {
+      const ts = records[id];
+
+      if (ts && ts < Date.now() - 3000) {
+        const dt = new Date(ts);
+        toast.current.show({
+          severity: 'info',
+          summary: 'Work has been downloaded',
+          detail: `RJ${id} has been downloaded at ${dt.toLocaleString()}.`,
+        });
+      }
+    }
+  }, [id]);
 
   const trackMap = useMemo(() => {
     const map = new Map<string, Track>();
@@ -57,22 +86,27 @@ function App() {
   }, [tracks]);
 
   useEffect(() => {
-    let id = rjid.toUpperCase();
-
-    if (id.startsWith('RJ')) {
-      id = id.substring(2);
-    }
-
     if (id) {
       setLoading(true);
       getTracks(id, proxy)
         .then(setTracks)
-        .catch(console.error)
+        .catch((e) => {
+          setTracks([]);
+          console.error(e);
+
+          if ('error' in e) {
+            toast.current?.show({
+              severity: 'error',
+              summary: 'Error',
+              detail: e.error,
+            });
+          }
+        })
         .finally(() => setLoading(false));
     } else {
       setTracks([]);
     }
-  }, [rjid, proxy]);
+  }, [id, proxy]);
 
   return (
     <main className="flex flex-column gap-2 h-full p-2">
@@ -120,7 +154,10 @@ function App() {
                       track.progress = progress;
                     }
                   },
-                  () => setChild(undefined),
+                  () => {
+                    setChild(undefined);
+                    setRecords((old) => ({ ...old, [id]: Date.now() }));
+                  },
                 );
                 setChild(c);
               }
@@ -129,6 +166,7 @@ function App() {
         />
       </div>
       <SettingsDialog visible={settingsVisible} onHide={() => setSettingsVisible(false)} />
+      <Toast ref={toast} />
     </main>
   );
 }
